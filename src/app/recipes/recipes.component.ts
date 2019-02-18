293e760f27166d8a73dc08060998c3f27d6cdb01
Jessie Huff <huffjd1@gmail.com>
2019-02-18 12:56:53 -0500

    Worked on setting up the visual structure of the application

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-recipe-book',
-  templateUrl: './recipe-book.component.html',
-  styleUrls: ['./recipe-book.component.css']
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
 })
-export class RecipeBookComponent implements OnInit {
+export class RecipesComponent implements OnInit {
 
   constructor() { }
 
