44cfa93f3a30d21a731cc7f61e0ffed1c1c715bc
Jessie Huff <huffjd1@gmail.com>
2019-03-06 13:49:17 -0500

    Creating an ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+    constructor(public name: string, public amount: number){} 
+    //will automatically give us properties with names we specify as argument names and assign the values to these properties
+
+}
